a9eb2cb3577b22bbbabb877b8c8a0bdca01d0ca4
Harsh Tiwari <59537489+tiwari-ai-harsh@users.noreply.github.com>
2023-03-01 21:16:56 +0530

    Made the nav bar textsize to be responsive too

--- components/nav/navLink.tsx
+++ components/nav/navLink.tsx
@@ -12,7 +12,7 @@ export default function NavLink({
   return (
     <Link
       href={to}
-      className={`mx-4 text-3xl font-semibold transition-all duration-300 ease-in-out group`}
+      className={`mx-4 text-2xl md:text-3xl font-semibold transition-all duration-300 ease-in-out group`}
     >
       <span
         className={`bg-left-bottom bg-gradient-to-r from-pink-500 to-red-400 bg-[length:0%_2px] bg-no-repeat ${
