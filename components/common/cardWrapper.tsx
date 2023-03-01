59dcf296c49536ea8ebcba301fded8cb54b80b21
Harsh Tiwari <59537489+tiwari-ai-harsh@users.noreply.github.com>
2023-03-01 21:15:14 +0530

    Made the text size to be responsive too

--- components/common/cardWrapper.tsx
+++ components/common/cardWrapper.tsx
@@ -7,7 +7,7 @@ export default function CardWrapper({
 }) {
   return (
     <div
-      className="p-16 mb-10 md:mb-32 border-2 bg-white border-black 
+      className="p-8 md:p-16 mb-10 md:mb-32 border-2 bg-white border-black 
     shadow-[10px_10px_0px_0px_rgba(255,235,183)] "
     >
       <div className="text-3xl font-semibold text-black capitalize">
--- components/home/interests.tsx
+++ components/home/interests.tsx
@@ -3,7 +3,7 @@ import CardWrapper from '../common/cardWrapper';
 export default function Interests() {
   return (
     <CardWrapper heading="Interests: ">
-      <div className="grid grid-cols-2 mt-5 text-4xl text-black capitalize indent-5">
+      <div className="grid grid-cols-1 mt-5 text-2xl text-black capitalize md:text-4xl md:grid-cols-2 indent-5">
         <div>
           <div className="font-semibold capitalize">Technology:</div>
           <div className="col-auto ml-4 gird">
@@ -15,7 +15,7 @@ export default function Interests() {
             <p>Node Js</p>
           </div>
         </div>
-        <div>
+        <div className="pt-5 md:pt-0">
           <div className="font-semibold capitalize">Miscellaneous:</div>
           <div className="col-auto ml-4 gird">
             <p>Guitar</p>
