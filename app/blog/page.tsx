1c886c94d00dc1c0ad64aa8d60c3a95c7e7ad72e
Harsh Tiwari <59537489+tiwari-ai-harsh@users.noreply.github.com>
2023-03-05 15:03:41 +0530

    Removed the animations

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,27 @@
 'use client';
 import CardWrapper from '@/components/common/cardWrapper';
 import { Inter } from '@next/font/google';
+import { motion } from 'framer-motion';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function BlogPage() {
   return (
-    <CardWrapper heading="Blogs:">
-      <div className="text-black">
-        My blogs will appear here. (When I'll eventually write some :/)
-      </div>
-    </CardWrapper>
+    <motion.div
+      initial={{ x: 300, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 300, opacity: 0 }}
+      transition={{
+        type: 'spring',
+        stiffness: 260,
+        damping: 20,
+      }}
+    >
+      <CardWrapper heading="Blogs:">
+        <div className="text-black">
+          My blogs will appear here. (When I'll eventually write some :/)
+        </div>
+      </CardWrapper>
+    </motion.div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
 'use client';
-import Scene from '@/components/3d/containers/scene';
 import AboutMe from '@/components/home/aboutMe';
 import Interests from '@/components/home/interests';
 import MeLinks from '@/components/home/meLinks';
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
     <div>
-      <Scene />
-      <AboutMe />
-      <Interests />
-      <MeLinks />
+      <motion.ul
+        initial={{ x: 300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: 300, opacity: 0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
+        }}
+      >
+        <motion.li>
+          <AboutMe />
+        </motion.li>
+        <motion.li>
+          <Interests />
+        </motion.li>
+        <motion.li>
+          <MeLinks />
+        </motion.li>
+      </motion.ul>
     </div>
   );
 }
